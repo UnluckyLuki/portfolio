80b929d6eee949dbd4596f3e323777539cf8e32d
UnluckyLuki <themrluki43@gmail.com>
2024-07-17 13:41:04 +0200

    Adding routes to gh-pages

--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {HashRouter as Router} from "react-router-dom";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Router>
-    <App />
-  </Router>
+    <React.StrictMode>
+        <Router>
+            <Routes>
+                <Route path="/portfolio/*" element={<App />} />
+            </Routes>
+        </Router>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
