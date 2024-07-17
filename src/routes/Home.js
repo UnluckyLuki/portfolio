0b9067cba421578a7d60ef69fe54f2ac1571718b
UnluckyLuki <themrluki43@gmail.com>
2024-07-17 13:20:48 +0200

    Adding routes to gh-pages

---
+++ src/routes/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Typography} from "@mui/material";
+
+const Home = () => {
+    return (
+        <div>
+            <Typography variant="body2" color="textSecondary">Hello</Typography>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/routes/MyProjects.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Typography} from "@mui/material";
+
+const MyProjects = () => {
+    return (
+        <div>
+            <Typography variant="body2" color="textSecondary">My Projects</Typography>
+        </div>
+    );
+};
+
+export default MyProjects;
