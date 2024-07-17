96c01095bc99b880317a4ac94f1670f5ad7eae40
UnluckyLuki <themrluki43@gmail.com>
2024-07-17 13:48:45 +0200

    Adding routes to gh-pages

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import './App.css';
 import {Route, Routes} from "react-router-dom";
 import Home from "./routes/Home";
 import MyProjects from "./routes/MyProjects";
+import NavBar from "./componets/NavBar";
 
 function App() {
-  return (
-    <div className="App">
-      <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/myProjects" element={<MyProjects />} />
-      </Routes>
-    </div>
-  );
+    return (
+        <div className="App">
+            <NavBar/>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/home" element={<Home/>}/>
+                <Route path="/myProjects" element={<MyProjects/>}/>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/componets/NavBar.js
@@ -0,0 +1,55 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import AccessibleForwardIcon from '@mui/icons-material/AccessibleForward';
+import {useNavigate} from "react-router-dom";
+
+const pages = ['home','myProjects'];
+
+function NavBar() {
+    const navigate = useNavigate()
+
+    return (
+        <AppBar position="static">
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                    <AccessibleForwardIcon sx={{display: {xs: 'none', md: 'flex'}, mr: 1}}/>
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        sx={{
+                            mr: 2,
+                            display: {xs: 'none', md: 'flex'},
+                            fontFamily: 'monospace',
+                            fontWeight: 700,
+                            letterSpacing: '.3rem',
+                            color: 'inherit',
+                            textDecoration: 'none',
+                        }}
+                    >
+                        MYPAGE
+                    </Typography>
+                    <Box sx={{flexGrow: 1, display: {xs: 'none', md: 'flex'}}}>
+                        {pages.map((page) => (
+                            <Button
+                                key={page}
+                                onClick={() => {
+                                    navigate(`/${page}`)
+                                }}
+                                sx={{my: 2, color: 'white', display: 'block'}}
+                            >
+                                {page}
+                            </Button>
+                        ))}
+                    </Box>
+                </Toolbar>
+            </Container>
+        </AppBar>
+    );
+}
+
+export default NavBar;
